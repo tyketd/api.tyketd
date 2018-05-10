545ccc612d82dd03809dfc5a40103140cd6d6449
Stratege <sdanicktakam@gmail.com>
2018-05-10 19:03:28 +0200

    Création des entites + migrations suite

---
+++ app/Http/Controllers/AnnounceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Anounce;
+use Illuminate\Http\Request;
+
+class AnnounceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Anounce  $anounce
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Anounce $anounce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Anounce  $anounce
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Anounce $anounce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Anounce  $anounce
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Anounce $anounce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Anounce  $anounce
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Anounce $anounce)
+    {
+        //
+    }
+}
